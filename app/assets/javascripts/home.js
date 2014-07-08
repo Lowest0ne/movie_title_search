74ed80dbcaef7c3a3495f2d032bd5d9cb4e824b2
Carl Schwope <schwope.carl@gmail.com>
2014-07-08 19:38:16 -0400

    refactor entire javascript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,50 +1,86 @@
-function OmdbApi( base_element )
+
+$(document).ready( function(){
+
+  var inputs = [
+     createElement( 'INPUT', [ ['type', 'text'] , [ 'name', 's' ] ] )
+    ,createElement( 'INPUT', [ [ 'type', 'submit' ] ] )
+  ];
+
+  var form = createElement( 'FORM' );
+
+  for ( var i = 0; i < inputs.length; ++i )
+    form.appendChild( inputs[i] );
+
+  var query = document.getElementById( 'query' );
+
+  query.appendChild( form );
+
+  $(document).on( 'submit', form, onClick );
+
+});
+
+function createElement( type, attributes = [] )
 {
-  this.base_element = base_element;
-
-  this.response = null;
-
-  this.title_field = null;
-
-  this.prepareForm();
+  var element = document.createElement( type );
+  for ( var i= 0; i < attributes.length; ++i )
+    element.setAttribute( attributes[i][0], attributes[i][1] );
+  return element;
 }
 
-OmdbApi.prototype.prepareForm = function()
-{
-  title_field = document.createElement( "INPUT" );
-  title_field.setAttribute( 'type', 'text' );
-  title_field.setAttribute( 'name', 's');
-
-  var submit_button = document.createElement( "INPUT" );
-  submit_button.setAttribute( 'type', 'submit' );
-
-  var form = document.createElement( "FORM" );
-  form.setAttribute( 'name', 'omdb_query' );
-  form.appendChild( title_field );
-  form.appendChild( submit_button );
-
-  this.base_element.appendChild( form );
-
-  $(document).on("submit", form , this.getRequest );
-}
-
-OmdbApi.prototype.getRequest = function( event )
+function onClick( event )
 {
   event.preventDefault();
 
-  // Send Request
-  var http = new XMLHttpRequest();
-  http.open("GET", "http://www.omdbapi.com/?s=" +  document.omdb_query.s.value , false);
-  http.send(null);
+  var params = {};
 
-  this.response = http.responseText;
+  for ( var i = 0; i < event.data.length; ++i )
+  {
+    if ( event.data[i].type != 'submit' )
+      params[ event.data[i].name ] = event.data[i].value;
+  }
 
-  alert( this.response );
+  getOmdb( params , titleSearch );
 }
 
-$('document').ready( function(){
+function getOmdb( data, success )
+{
+  $.getJSON( 'http://www.omdbapi.com', data, success );
+}
 
-  var query_element = document.getElementById( 'query' );
-  var omdb_query = new OmdbApi( query_element );
+function titleSearch( data, status, jq )
+{
+  if ( !data["Search"] ) return;
 
-});
+  var search = data["Search"]
+
+  var result_container = createElement( 'DIV' );
+
+  for ( var i = 0; i < search.length; ++i )
+    result_container.appendChild( createMovieView( search[i] ) );
+
+  var result = document.getElementById( 'result' );
+
+  result.innerHTML = '';
+  result.appendChild( result_container );
+}
+
+function createMovieView( data )
+{
+  var container = createElement( 'DIV' );
+
+  var title = createElement( 'H3' )
+  title.innerHTML = data.Title;
+  container.appendChild( title );
+
+  getOmdb( { i: data.imdbID }, function( data, status, unused )
+  {
+    container.appendChild( createElement( 'IMG', [['src',data.Poster]]) );
+
+    var plot = createElement( 'P' );
+    plot.innerHTML = data.Plot;
+    container.appendChild( plot );
+  });
+
+
+  return container;
+}
